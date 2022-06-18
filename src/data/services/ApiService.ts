212907e53441610cc3d5306c923519109b950901
Luiz Gustavo <luiz1gustavo2@hotmail.com>
2022-06-18 19:40:48 -0300

    baseURL update

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const ApiService = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'https://adote-um-pet-hazel.vercel.app/',
     headers: {
         'Content-Type': 'application/json'
     }
